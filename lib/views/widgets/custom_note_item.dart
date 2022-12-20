005df333cd698378fc1eba389b3e444f3a131b02
osamaharoon <osama.lawyer111@example.com>
2022-12-20 15:52:37 -0800

    delet notes done

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -44,7 +44,9 @@ class NoteItem extends StatelessWidget {
                     )),
               ),
               trailing: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  notes.delete();
+                },
                 icon: const Icon(
                   FontAwesomeIcons.trash,
                   color: Colors.black,
